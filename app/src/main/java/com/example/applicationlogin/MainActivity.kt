d76772a5cf83367da56000ced500399ee9ed05cf
aldairyasser <aldair.meza@finwave.es>
2025-02-03 16:04:01 +0100

    feature/list

--- app/src/main/java/com/example/applicationlogin/MainActivity.kt
+++ app/src/main/java/com/example/applicationlogin/MainActivity.kt
@@ -4,44 +4,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.applicationlogin.ui.theme.ApplicationLoginTheme
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ApplicationLoginTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+            val navController = rememberNavController() // Controlador de navegación
+
+            NavHost(
+                navController = navController,
+                startDestination = "Login" // Pantalla inicial
+            ) {
+                composable("Login") {
+                    LoginScreen(
+                        onSuccess = { userName ->
+                            navController.navigate("Chat")
+                        }
                     )
                 }
+                composable("Chat") { ChatScreen() }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ApplicationLoginTheme {
-        Greeting("Android")
     }
 }
